15a1df9ef17a493a9c0da2d4671e7f060f2adfbe
abdo2017 <abdo.esam2020@yandex.com>
2019-07-20 02:51:18 +0200

    configured keys file

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://abdo:abdo.esam3@ds151817.mlab.com:51817/storybooks',
+  googleClientID:'563738166179-th08ptt8onal11gsuo1rfogj3e03blhs.apps.googleusercontent.com',
+  googleClientSecret:'7h32Dwbglww1LRNss0z-J3be'
+}
